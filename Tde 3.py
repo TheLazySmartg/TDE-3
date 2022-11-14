9b5241ca17001df1f9d1dae73558c2cc3559da40
SMARTG <51825210+TheLazySmartg@users.noreply.github.com>
2022-11-14 17:10:03 -0300

    Add files via upload

---
+++ Tde 3.py
@@ -0,0 +1,113 @@
+# Exercicio_1
+
+# def Qualquer():
+#     n = str(input("Digite: "))
+#     q = len(n)
+#     print(f"A quantidade de caracteres é {q}.")
+#
+# Qualquer()
+
+# Exercicio_2
+
+# n = int(input("Digite: "))
+#
+#
+# def Qualquer():
+#     if n % 2 == 0:
+#         print(True)
+#     else:
+#         print(False)
+#     return n
+#
+# Qualquer()
+
+# Exercicio_3
+
+# def Qualquer(a):
+#
+#     p = 'P'
+#     n = 'N'
+#     if a > 0:
+#         return p
+#     if a <= 0:
+#         return n
+# a = int(input("Digite: "))
+#
+# print(Qualquer(a))
+
+# Exercicio_4
+
+# def Qualquer(x):
+#     return (len(str(x)))
+# n = int(input("Digite: "))
+# print(Qualquer(n))
+
+# Exercicio_5
+
+# def Qualquer(a,b):
+#
+#     s = a + b
+#     return s
+#
+# a = str(input("Digite com espaço no final: "))
+# b = str(input("Digite: "))
+# print(Qualquer(a, b))
+
+# Exercicio_6
+
+# Lista = []
+#
+# while True:
+#     Lista.append(str(input("Digite: ")))
+#     r = str(input("Digite S/N: ")).upper()
+#     if r == 'N':
+#         break
+#
+# def Qualquer(Lista):
+#
+#     for linha in Lista:
+#         print(linha)
+#
+# Qualquer(Lista)
+
+# Exercicio_7
+
+# def Qualquer():
+#     c = int(input("Digite o valor da circuferencia: "))
+#     r = c / (2 * 3.14)
+#     return r
+#
+#
+# print('O valor do raio é:', Qualquer())
+
+# Exercicio_8
+
+# def Qualquer():
+#     nada = ''
+#     n = str(input("Digite: ")).upper()
+#     q = str(input("Qual letra você quer contar? ")).upper()
+#     print(f"A quantidade de vezes que aparece a letra {q} são {n.count(q)}")
+#     return nada
+# print(Qualquer())
+
+# Exercicio_9
+
+# def Qualquer(x):
+#     s = x * 0.1
+#     return s
+#
+#
+# n = int(input("Digite: "))
+# print(Qualquer(n), 'R$')
+
+# Exercicio_10
+
+# def Qualquer(a):
+#     lista = []
+#     for i in range(a):
+#         lista.append(0)
+#     return lista
+# 
+#
+# a = int(input("Digite: "))
+# print(Qualquer(a))
